30cd25582bdc62e603e0602f67271ca530292922
Mathias Grimm <mathiasgrimm@gmail.com>
2025-09-26 10:57:33 -0300

    wip

--- resources/views/livewire/file-upload-test.blade.php
+++ resources/views/livewire/file-upload-test.blade.php
@@ -1,9 +1,22 @@
 <div>
-    <input
-        type="file"
-        wire:model="theFile"
-    />
-    <br>
+    <div
+        x-data="{ uploading: false, progress: 0 }"
+        x-on:livewire-upload-start="uploading = true"
+        x-on:livewire-upload-finish="uploading = false"
+        x-on:livewire-upload-cancel="uploading = false"
+        x-on:livewire-upload-error="uploading = false"
+        x-on:livewire-upload-progress="progress = $event.detail.progress"
+    >
+        <input
+            type="file"
+            wire:model="theFile"
+        />
+
+        <!-- Progress Bar -->
+        <div x-show="uploading">
+            <progress max="100" x-bind:value="progress"></progress>
+        </div>
+    </div>
 
     <div>
         @if ($theFile)
