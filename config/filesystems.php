9b803c80871ab9de4780b27052019be42bdeb1b5
Mathias Grimm <mathiasgrimm@gmail.com>
2025-11-17 17:38:06 -0300

    wip

--- config/filesystems.php
+++ config/filesystems.php
@@ -58,6 +58,10 @@ return [
             'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
             'report' => false,
+            'options' => [
+                'mup_threshold' => 1024 * 1024 * 64,
+                'concurrency' => 100,
+            ],
         ],
 
     ],
